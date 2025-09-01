69ce34df8ebd0f5a8a61a4c24a228b8b6e65e72c
Saptamesh18 <129535043+Saptamesh18@users.noreply.github.com>
2025-09-01 17:16:52 +0530

    Update tb.v

--- tb.v
+++ tb.v
@@ -1,5 +1,3 @@
-// Dual Port RAM testbench
-
 module dual_port_ram_tb;
   reg [7:0] data_a, data_b; //input data
   reg [5:0] addr_a, addr_b; //Port A and Port B address
@@ -19,56 +17,60 @@ module dual_port_ram_tb;
     .q_b(q_b)
   );
   
-  initial
-    begin
-      $dumpfile("dump.vcd");
-      $dumpvars(1, dual_port_ram_tb);       
-      
-      clk=1'b1;
-      forever #5 clk = ~clk;
-    end
+  // Clock generation
+  initial begin
+    clk = 1'b1;
+    forever #5 clk = ~clk;
+  end
   
-  initial
-    begin
-      data_a = 8'h33;
-      addr_a = 6'h01;
-      
-      data_b = 8'h44;
-      addr_b = 6'h02;
-      
-      we_a = 1'b1;
-      we_b = 1'b1;
-      
-      #10;
-      
-      data_a = 8'h55;
-      addr_a = 6'h03;
-      
-      addr_b = 6'h01;
-      
-      we_b = 1'b0;
-      
-      #10;          
-            
-      addr_a = 6'h02;
-      
-      addr_b = 6'h03;
-      
-      we_a = 1'b0;
-      
-      #10;
-      
-      addr_a = 6'h01;
-      
-      data_b = 8'h77;
-      addr_b = 6'h02;
-      
-      we_b = 1'b1;
-      
-      #10;
-    end
+  // Stimulus
+  initial begin
+    data_a = 8'h33;
+    addr_a = 6'h01;
+    
+    data_b = 8'h44;
+    addr_b = 6'h02;
+    
+    we_a = 1'b1;
+    we_b = 1'b1;
+    
+    #10;
+    
+    data_a = 8'h55;
+    addr_a = 6'h03;
+    
+    addr_b = 6'h01;
+    
+    we_b = 1'b0;
+    
+    #10;          
+    
+    addr_a = 6'h02;
+    
+    addr_b = 6'h03;
+    
+    we_a = 1'b0;
+    
+    #10;
+    
+    addr_a = 6'h01;
+    
+    data_b = 8'h77;
+    addr_b = 6'h02;
+    
+    we_b = 1'b1;
+    
+    #10;
+  end
   
-  initial	
-    #40 $stop;
+  // Monitor outputs
+  initial begin
+    $monitor("Time=%0t | A: we=%b addr=%h data=%h q_a=%h || B: we=%b addr=%h data=%h q_b=%h",
+             $time, we_a, addr_a, data_a, q_a, we_b, addr_b, data_b, q_b);
+  end
+  
+  // End simulation
+  initial #40 $stop;
   
 endmodule
+
